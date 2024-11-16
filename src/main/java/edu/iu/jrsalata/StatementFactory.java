f7b6c3d4705710800c3ce01a035b0c0cc46d04a9
John S <51085651+sheepman39@users.noreply.github.com>
2024-11-16 19:47:28 +0000

    Moved handleAsmStatement to parent

--- src/main/java/edu/iu/jrsalata/StatementFactory.java
+++ src/main/java/edu/iu/jrsalata/StatementFactory.java
@@ -26,7 +26,7 @@ public class StatementFactory extends AbstractStatementFactory {
 
         // flag for format 4
         boolean eFlag = false;
-        
+
         // get the parts of the statement
         String[] parts = splitStatement(statement);
         String mnemonic = parts[0];
@@ -71,87 +71,23 @@ public class StatementFactory extends AbstractStatementFactory {
         return newStatement;
     }
 
-    private void handleByte(String args, DirectiveStatement statement) throws InvalidAssemblyFileException {
-        // check if the first char is C or X
-        // C represents a constant string whose length is the length of the string
-        // the object code of C is the ASCII value of each character in the string
-        // X represents an object code whose length is 1 and the object code is the arg
-        if (args.charAt(0) == 'C') {
-
-            // remove the C and the ' at the end
-            args = args.substring(2, args.length() - 1);
-
-            // set the size to the length of the string
-            statement.setSize(new HexNum(args.length()));
-
-            // set the object code to the ASCII value of each character
-            String objCode = "";
-            for (int i = 0; i < args.length(); i++) {
-                objCode += Integer.toHexString(args.charAt(i));
-            }
-            statement.setObjCode(objCode);
-
-        } else if (args.charAt(0) == 'X') {
-
-            // remove the X and the ' at the end
-            args = args.substring(2, args.length() - 1);
-
-            // set the size to 1
-            statement.setSize(new HexNum(1));
-
-            // set the object code to the arg
-            statement.setObjCode(args);
-
-        } else {
-            StringBuilder msg = new StringBuilder("Invalid BYTE argument: ");
-            msg.append(args);
-            throw new InvalidAssemblyFileException(lineNum, msg.toString());
-        }
-    }
-
-    private Statement handleAsmStatement(String mnemonic, String args) throws InvalidAssemblyFileException {
+    @Override
+    protected Statement handleAsmStatement(String mnemonic, String args) throws InvalidAssemblyFileException {
 
         DirectiveStatement returnVal = new DirectiveStatement();
         returnVal.setDirective(mnemonic);
-        switch (mnemonic) {
-            case "START":
-                this.locctr = new HexNum(args, NumSystem.HEX);
-                this.start = new HexNum(this.locctr.getDec());
-                break;
-            case "END":
-                // do nothing
-                break;
-            case "BYTE":
-                // move BYTE logic to other method for cleanliness
-                handleByte(args, returnVal);
-                break;
-            case "WORD":
-                // set size to 3 and set the object code
-                returnVal.setSize(new HexNum(3));
-                returnVal.setObjCode(new HexNum(args, NumSystem.DEC).toString(6));
-                break;
-            case "RESB":
-                // set the args to the size
-                returnVal.setSize(new HexNum(args, NumSystem.DEC));
-                break;
-            case "RESW":
-                // set 3 * args to the size
-                returnVal.setSize(new HexNum(3 * Integer.parseInt(args)));
-                break;
-            case "BASE":
-                // set the base to the args
-                // will be passed to the ExtendedStatement
-                this.base = args;
-                break;
-            case "NOBASE":
-                // clear the base
-                this.base = "";
-            default:
-                StringBuilder msg = new StringBuilder("Invalid ASM mnemonic: ");
-                msg.append(mnemonic);
-                throw new InvalidAssemblyFileException(lineNum, msg.toString());
+
+        // check for BASE or NOBASE directive first
+        if (mnemonic.equals("BASE")) {
+            this.base = args;
+            return returnVal;
+        } else if (mnemonic.equals("NOBASE")) {
+            this.base = "";
+            return returnVal;
         }
-        return returnVal;
+
+        // then use the parent for the rest
+        return super.handleAsmStatement(mnemonic, args);
     }
 
     private Statement createStatement(String mnemonic, String args) {
