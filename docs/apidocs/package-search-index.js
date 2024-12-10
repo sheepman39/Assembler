113b4dbea1ecf090b1a91fc78c9b757c6ec36835
John Salata <51085651+sheepman39@users.noreply.github.com>
2024-12-10 21:46:49 +0000

    Formatter

--- docs/apidocs/package-search-index.js
+++ docs/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.iu.jrsalata"}];updateSearchResults();
+packageSearchIndex = [{ "l": "All Packages", "u": "allpackages-index.html" }, { "l": "edu.iu.jrsalata" }]; updateSearchResults();
